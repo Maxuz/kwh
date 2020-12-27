af5d65e5bbfebc877122b43d577aba719694feb9
maxuz <maxuz@protonmail.ch>
2020-12-27 15:33:43 +0700

    Localization

---
+++ src/main/java/dev/maxuz/kwh/config/AppConfig.java
@@ -0,0 +1,25 @@
+package dev.maxuz.kwh.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("ru"));
+        return slr;
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("locale");
+        return lci;
+    }
+}
